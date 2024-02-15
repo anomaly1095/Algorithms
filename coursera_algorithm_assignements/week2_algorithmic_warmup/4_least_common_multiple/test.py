32196929725062873a678c26b76de1b146ab93a7
Somali99 <syoyo1095@gmail.com>
2024-02-15 23:12:35 +0100

    untrackzed coursera and object files

--- coursera_algorithm_assignements/week2_algorithmic_warmup/4_least_common_multiple/test.py
+++
@@ -1,9 +0,0 @@
-def lcm(a, b):
-    return abs(a * b) // gcd(a, b)
-
-def main():
-    a, b = map(int, input().split())
-    result = lcm(a, b)
-    print(f"The LCM of {a} and {b} is {result}")
-if __name__ == '__main__':
-    main()
