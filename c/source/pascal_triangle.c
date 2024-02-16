b03f53f2f72f6d1f3df0bfc18320901a4dd18df3
Somali99 <syoyo1095@gmail.com>
2024-02-16 02:05:08 +0100

    pascal triangle ready but needs modificxation for larger values for perfect triangle display

--- c/source/pascal_triangle.c
+++ c/source/pascal_triangle.c
@@ -2,50 +2,78 @@
 #include <stdlib.h>
 
 // x are the rows and y are the columns
-void fill_element(int **arr, int rows, int x, int y){
+void fill_element(int **arr, int rows, int x, int y) {
+    if (y == 0 || y == x) {
+        *(*(arr + x) + y) = 1;
+    } else {
+        *(*(arr + x) + y) = *(*(arr + x - 1) + y - 1) + *(*(arr + x - 1) + y);
+    }
 
-    if (y == 0 || y == x){  // we take care of case where items have not parents or 
-        *(arr+x)[y] = 1;    // one of the parents are missing
-    }else{
-        *(arr+x)[y] = *(arr+x-1)[y-1] + *(arr+x-1)[y]; // element is equal to both elements on top of it
-    }
-    
-    //deciding to go right or to go down or end
-    if (x == rows-1 && y == x){ // reached last item in triangle
+    // deciding to go right or to go down or end
+    if (x == rows - 1 && y == x) {
         return;
-    }
-    else if (x != rows-1){ // reached last item in row
-        fill_element(arr, rows, x+1, 0);
-    }
-    else{
-        fill_element(arr, rows, x, ++y); 
+    } else if (y == x) {
+        fill_element(arr, rows, ++x, 0); // increment x, not y
+    } else {
+        fill_element(arr, rows, x, ++y);
     }
 }
 
-int **pascal_triangle(int rows){
+
+int **mem_alloc(int rows){
     if (rows == 1) {
-        int **arr = (int **)calloc(sizeof(int *), 1);
+        int **arr = (int**)calloc(sizeof(int*), rows);
         *arr = (int *)calloc(sizeof(int), 1);
-        *(arr)[0] = 1;
+        *(*(arr)) = 1;
         return arr;
     }
     int i, **arr = (int**)calloc(sizeof(int*), rows); // allocating a list of pointers
 
     for (i = 0; i < rows; i++){
-        *(arr+i) = (int*)calloc(sizeof(int), i+1); // allocating the memory fo each pointer
+        *(arr+i) = (int*)calloc(sizeof(int), i+1); // allocating the memory for each pointer
     }
-    fill_element(arr, rows, 0, 0);
+    return arr;
+}
+
+int **pascal_triangle(int rows){
+
+    int **arr = mem_alloc(rows); // allocate the memory for the triangle
+    fill_element(arr, rows, 0, 0); // populate the triangle with the correct values
     return arr;
 }
 
 
+void print_triangle(int **arr, int rows){
+    if (rows == 1){
+        printf("1\n");
+    }
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < (rows-i); j++){ // printing spaces
+            printf(" ");
+        }
+        for (int t = 0; t < i+1; t++){
+            printf("%d ", *(*(arr+i)+t));
+        }
+        printf("\n");
+    }
+}
+
+void mem_dealloc(int **arr, int rows) {
+    for (int i = 0; i < rows; i++) {
+        free(arr[i]);
+    }
+    free(arr);
+}
 
 int main(int argc, char **argv){
-    // if (argc != 2){
-    //     return -1;
-    // }
-    // int **arr = pascal_triangle(atoi(argv[1]));
-    int **arr = pascal_triangle(5);
+    if (argc != 2){
+        return -1;
+    }
+    int rows = atoi(argv[1]);
+    int **arr = pascal_triangle(rows);
+    print_triangle(arr, rows);
+    
+
     free(arr);
     return 0;
 }
