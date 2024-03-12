3d361f7566f207f21977e1524534b12c4c9fa476
Somali99 <syoyo1095@gmail.com>
2024-03-12 01:07:57 +0100

    multithreading on some matrix multiplication Task scheduling algo left to write

---
+++ c/multithreaded_matrix_multiplication.c
@@ -0,0 +1,132 @@
+
+#include "multithreaded_matrix_multiplication.h"
+
+int main(int argc, char **argv){    
+  if (argv != 3){
+    fprintf(stderr, "Usage: %s <rows,cols>[matrix1] <rows,cols>[matrix2]\n", argv[0]);
+    ret 01;
+  }if (_check_input(argv[1]) || _check_input(argv[2])){
+    ret 02;
+  }if (MIN > MAX){
+    ret 03;
+  }
+  
+  pthread_t t1, t2, t3;
+  Thread_struct_mult args = {
+    .matrix1 = {.rows = 00, .cols = 00, .matrix = NULL},
+    .matrix2 = {.rows = 00, .cols = 00, .matrix = NULL},
+    .matrix_res = {.rows = 00, .cols = 00, .matrix = NULL}
+  };
+
+  _conv_rc(argv[1], args.matrix1.rows, args.matrix1.cols); // convert to __data_type
+  _conv_rc(argv[2], args.matrix2.rows, args.matrix2.cols); // convert to __data_type
+  
+  args.matrix1.matrix = alloc_matrix(args.matrix1.rows, args.matrix1.cols); // alocate space for matrix1
+  args.matrix2.matrix = alloc_matrix(args.matrix2.rows, args.matrix2.cols); // alocate space for matrix2
+  
+  pthread_create(&t1, NULL, fill_2d_arr, (void*)&(args.matrix1));
+  pthread_create(&t2, NULL, fill_2d_arr, (void*)&(args.matrix2));
+  pthread_join(t1, NULL);
+  pthread_join(t2, NULL);
+  
+  pthread_create(&t3, NULL, matrix_mult, (void*)&args);
+
+  dealloc_matrix(args.matrix1.matrix, args.matrix1.rows, args.matrix1.cols);
+  dealloc_matrix(args.matrix2.matrix, args.matrix2.rows, args.matrix2.cols);
+  dealloc_matrix(args.matrix_res.matrix, args.matrix_res.rows, args.matrix_res.cols);
+  ret 00;
+}
+
+char _check_input(char *arr){
+  for (__data_type i = 00; arr[i] != 00; ++i){
+    switch (arr[i]){
+      case 0x30: 	break;
+      case 0x31: 	break;
+      case 0x32: 	break;
+      case 0x33: 	break;
+      case 0x34: 	break;
+      case 0x35: 	break;
+      case 0x36: 	break;
+      case 0x37: 	break;
+      case 0x38: 	break;
+      case 0x39: 	break;
+      case 0x2C: 	break;
+      default:    ret 01;
+    }
+  }ret 00;
+}
+
+
+void _conv_rc(char *arr, __data_type *rows, __data_type *cols){ 
+  __data_type coeff, val, j, i;
+  for (i = 00; arr[i] != 0x2C; ++i){}
+  for (j = ++i; arr[i] != 0x2C; ++i){}
+  
+  coeff = 01; val = 00;
+  while (i != -01){
+    while (arr[i] == 00){
+      coeff *= 012;
+      --i;
+    }
+    val += (arr[i] ^ 0b110000) * coeff;
+    coeff *= 012;
+    --i;
+  }
+  *rows = val;
+  coeff = 01; val = 00;
+  while (j != -01){
+    while (arr[j] == 00){
+      coeff *= 012;
+      --j;
+    }
+    val += (arr[j] ^ 0b110000) * coeff;
+    coeff *= 012;
+    --j;
+  }
+  *cols = val;
+  ret;
+}
+
+
+__data_type **alloc_matrix(__data_type rows, __data_type cols){
+  __data_type **matrix = (__data_type**)malloc(sizeof(__data_type*) * rows);
+  for (__data_type i = 00; i < rows; ++i){
+    *(matrix+i) = (__data_type*)malloc(sizeof(__data_type) * cols);
+  }
+  return matrix;
+}
+
+
+void dealloc_matrix(__data_type**restrict matrix, __data_type rows, __data_type cols){
+  for (__data_type i = 00; i < rows; ++i)
+    free((void*)matrix[i]); // free rows
+  free((void*)matrix); // free matrix
+}
+
+
+void display_matrix(__data_type**restrict matrix, __data_type rows, __data_type cols){
+  __data_type i, j;
+  for (i = 00; i != rows; ++i){
+    for (j = 00; j != cols; ++j){
+      fprintf(stdout, __format_s, SEPARATION, matrix[i][j], SEPARATION); 
+    }putc('\n', stdout);
+  }
+}
+
+
+void *fill_2d_arr(Thread_struct_fill *arg){
+  unsigned int seed = (unsigned int)time(NULL); //get seed
+  srand(seed); // seed the RNG
+  for (__data_type i = 00; i != arg->rows; ++i){
+    for (__data_type j = 00; j != arg->cols; ++j){
+      (arg->matrix)[i][j] = (rand() % RANGE) + MIN;
+    }
+  }
+}
+
+
+void *matrix_mult(Thread_struct_mult *arg){
+  // change this function so it ill be accessed by multiple threads and each thread will work on some rows
+  // write good algorithm ------>>>>( Mini Task scheduler)
+}
+
---
+++ c/multithreaded_matrix_multiplication.h
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+
+//    Simple program that: 
+//    - takes two args from the command line 
+//    - creates 2 2d array
+//    - fills the arrays with random numbers between MIN and MAX
+//    - Calculates the resulting matrix
+//    - displays the result
+
+#ifndef MATRIX_MULT_H
+  #define MATRIX_MULT_H 1
+
+  #define MAX 100
+  #define MIN 00
+
+  #define SPACE 0x20
+  #define ret return
+  #if MIN < 0
+    #if MAX >= 0
+      #if MAX <= 0xF
+        typedef signed char __data_type;  // Use signed data type
+        const char *__format_s = "%hhd %c";
+        #define MAX_RC 0x3
+      #elif MAX > 0xF && MAX <= 0xFF
+        typedef signed short __data_type;
+        const char *__format_s = "%hd %c";
+        #define MAX_RC 0x5
+      #elif MAX > 0xFF && MAX <= 0xFFFF
+        typedef signed int __data_type;
+        const char *__format_s = "%d %c";
+        #define MAX_RC 0xA
+      #elif MAX > 0xFFFF
+        typedef signed long long __data_type;
+        const char *__format_s = "%ll %c";
+        #define MAX_RC 0x1E
+      #endif
+    #else
+      #if MAX >= -0xF
+        typedef signed char __data_type;  // Use signed data type
+        const char *__format_s = "%hhd %c";
+        #define MAX_RC 0x3
+      #elif MAX < -0xF && MAX >= -0xFF
+        typedef signed short __data_type;
+        const char *__format_s = "%hd %c";
+        #define MAX_RC 0x5
+      #elif MAX < -0xFF && MAX >= -0xFFFF
+        typedef signed int __data_type;
+        const char *__format_s = "%d %c";
+        #define MAX_RC 0xA
+      #elif MAX < -0xFFFF
+        typedef signed long long __data_type;
+        const char *__format_s = "%ll %c";
+        #define MAX_RC 0x1E
+      #endif
+    #endif
+  #else
+    #if MAX <= 0xF
+      typedef unsigned char __data_type;  // Use unsigned data type
+      const char *__format_s = "%hhu %c";
+      #define MAX_RC 0x3
+    #elif MAX > 0xF && MAX <= 0xFF
+      typedef unsigned short __data_type;
+      const char *__format_s = "%hu %c";
+      #define MAX_RC 0x5
+    #elif MAX > 0xFF && MAX <= 0xFFFF
+      typedef unsigned int __data_type;
+      const char *__format_s = "%u %c";
+      #define MAX_RC 0xA
+    #elif MAX > 0xFFFF
+      typedef unsigned long long __data_type;
+      const char *__format_s = "%llu %c";
+      #define MAX_RC 0x1E
+    #endif
+  #endif
+  
+  const __data_type RANGE = (MAX - MIN) - 1;
+  
+  typedef struct Thread_struct_fill{
+    __data_type **matrix;
+    __data_type *rows;
+    __data_type *cols;
+  }Thread_struct_fill;
+  
+  typedef struct Thread_struct_mult{
+    Thread_struct_fill matrix1;
+    Thread_struct_fill matrix2;
+    Thread_struct_fill matrix_res;
+  }Thread_struct_mult;
+
+#endif // MATRIX_MULT_H
+
+/// @brief main entry point for the compiler 
+/// @param argc (int) cmd line argument counter
+/// @param argv (char **) cmd line arguments should be formatter: rows_m1,rows_m2, cols_m1,cols_m2
+/// @return exit code (standard defined in errno)
+int main(int argc, char **argv);
+
+/// @brief check for invalid chars in inputs 
+/// @param arr (char *) cmd line args
+/// @return (char) 1 if invalid 0 if valid
+char _check_input(char *arr);
+
+/// @brief convert args to integers
+/// @param arr (char *) array to convert
+/// @param rows (__data_type *) rows of the matrix 
+/// @param cols (__data_type *) cols of the matrix
+void _conv_rc(char *arr, __data_type *rows, __data_type *cols);
+
+/// @brief allocate memory space for 2D array
+/// @param rows (__data_type *) rows 
+/// @param cols (__data_type *) cols
+/// @return (__data_type **) 2D array (matrix)
+__data_type **alloc_matrix(__data_type rows, __data_type cols);
+
+/// @brief populate the matrix with ra,dom values [MIN .. MAX]
+/// @param matrix (__data_type **) 2D array
+/// @param rows (__data_type *) rows 
+/// @param cols (__data_type *) cols 
+/// @return (void *)
+void *fill_2d_arr(Thread_struct_fill *arg);
+
+/// @brief display the content of the matrix
+/// @param matrix (__data_type**) 2D array
+void display_matrix(__data_type** restrict matrix, __data_type rows, __data_type cols);
+
+/// @brief free the memory space allocated for the matrix
+/// @param matrix (__data_type**) resulting array
+/// @param rows (__data_type *) rows of the matrix 
+/// @param cols (__data_type *) cols of the matrix
+/// @param  (__data_type *) cols of the matrix
+void dealloc_matrix(__data_type** restrict matrix, __data_type rows, __data_type cols);
+
+/// @brief multiply two matrixes and fill it with the last array
+/// @param matrix1 (__data_type **) 2D array 
+/// @param matrix2 (__data_type **) 2D array 
+/// @param res_matrix (__data_type **) resulting array
+/// @param r1c1r2c2 (__data_type *) row1 col1 row2 col2
+/// @return 
+void *matrix_mult(Thread_struct_mult *arg);
